123ccf820f1347cf9717a3c9fa24cbb2db54f8e4
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-11-21 18:19:11 -0800

    add meta tag

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <GoogleOAuthProvider clientId={"639818267081-06fchc8fuc3uu5laf1sjb4csb1pr26k2.apps.googleusercontent.com"}>
-    <App />
-  </GoogleOAuthProvider>
+  <App />
 );
